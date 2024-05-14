682b637a641c7eccd3f22dd2d3f6b499123d224d
cv-moon <cristian.chuquitarco92@gmail.com>
2024-05-13 21:01:46 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace cchuquitarcoT6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

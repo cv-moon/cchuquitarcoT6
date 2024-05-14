6c60e35dc42c2af8f131af33abdf7daa3dd0c3ff
cv-moon <cristian.chuquitarco92@gmail.com>
2024-05-14 16:42:54 -0500

    Realizando cambios, se present error de debbuging.

---
+++ Views/vEstudiante.xaml.cs
@@ -0,0 +1,25 @@
+using cchuquitarcoT6.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace cchuquitarcoT6.Views;
+
+public partial class vEstudiante : ContentPage
+{
+	private const string url = "http://10.2.199.46/moviles/wsestudiantes.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Estudiante> est;
+	public vEstudiante()
+	{
+		InitializeComponent();
+		ObtenerDatos();
+	}
+
+	public async void ObtenerDatos()
+	{
+		var content = await cliente.GetStringAsync(url);
+		List<Estudiante> mostrar = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+		est = new ObservableCollection<Estudiante>(mostrar);
+		listEstudiante.ItemsSource = est;
+	}
+}

1f3f74097624ff6028a333dbcf6a77b5818e9302
cv-moon <cristian.chuquitarco92@gmail.com>
2024-05-15 20:43:32 -0500

    actividad en clase.

---
+++ Views/vActEliminar.xaml.cs
@@ -0,0 +1,25 @@
+using cchuquitarcoT6.Models;
+
+namespace cchuquitarcoT6.Views;
+
+public partial class vActEliminar : ContentPage
+{
+    public vActEliminar(Estudiante datos)
+    {
+        InitializeComponent();
+        txtCodigo.Text = datos.codigo.ToString();
+        txtNombre.Text = datos.nombre;
+        txtApellido.Text = datos.apellido;
+        txtEdad.Text = datos.edad.ToString();
+    }
+
+    private void btnActualizar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnEliminar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
--- Views/vEstudiante.xaml.cs
+++ Views/vEstudiante.xaml.cs
@@ -6,20 +6,31 @@ namespace cchuquitarcoT6.Views;
 
 public partial class vEstudiante : ContentPage
 {
-	private const string url = "http://192.168.3.11/moviles/wsestudiantes.php";
-	private readonly HttpClient cliente = new HttpClient();
-	private ObservableCollection<Estudiante> est;
-	public vEstudiante()
-	{
-		InitializeComponent();
-		ObtenerDatos();
-	}
+    private const string url = "http://192.168.3.11/moviles/wsestudiantes.php";
+    private readonly HttpClient cliente = new HttpClient();
+    private ObservableCollection<Estudiante> est;
+    public vEstudiante()
+    {
+        InitializeComponent();
+        ObtenerDatos();
+    }
 
-	public async void ObtenerDatos()
-	{
-		var content = await cliente.GetStringAsync(url);
-		List<Estudiante> mostrar = JsonConvert.DeserializeObject<List<Estudiante>>(content);
-		est = new ObservableCollection<Estudiante>(mostrar);
-		listEstudiante.ItemsSource = est;
-	}
+    public async void ObtenerDatos()
+    {
+        var content = await cliente.GetStringAsync(url);
+        List<Estudiante> mostrar = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+        est = new ObservableCollection<Estudiante>(mostrar);
+        listEstudiante.ItemsSource = est;
+    }
+
+    private void btnAgregar_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new vAgregar());
+    }
+
+    private void listEstudiante_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var objEstudiante = (Estudiante)e.SelectedItem;
+        Navigation.PushAsync(new vActEliminar(objEstudiante));
+    }
 }

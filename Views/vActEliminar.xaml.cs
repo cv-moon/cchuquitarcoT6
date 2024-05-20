b0a684a79d68f2c9801e49792aa84dc8011141ce
cv-moon <cristian.chuquitarco92@gmail.com>
2024-05-19 21:38:01 -0500

    implementando actualizar y eliminar

--- Views/vActEliminar.xaml.cs
+++ Views/vActEliminar.xaml.cs
@@ -1,9 +1,11 @@
 using cchuquitarcoT6.Models;
+using System.Net;
 
 namespace cchuquitarcoT6.Views;
 
 public partial class vActEliminar : ContentPage
 {
+    string url = "http://192.168.100.109/moviles/wsestudiantes.php";
     public vActEliminar(Estudiante datos)
     {
         InitializeComponent();
@@ -15,11 +17,38 @@ public partial class vActEliminar : ContentPage
 
     private void btnActualizar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("nombre", txtNombre.Text);
+            parametros.Add("apellido", txtApellido.Text);
+            parametros.Add("edad", txtEdad.Text);
 
+            cliente.UploadValues($"{url}?codigo={txtCodigo.Text}", "PUT", parametros);
+            DisplayAlert("Alerta!", cliente.ToString(), "Cerrar");
+            Navigation.PushAsync(new vEstudiante());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Alerta!", ex.Message, "Cerrar");
+        }
     }
 
     private void btnEliminar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            //parametros.Add("codigo", txtCodigo.Text);
 
+            cliente.UploadValues($"{url}?codigo={txtCodigo.Text}", "DELETE", parametros);
+            Navigation.PushAsync(new vEstudiante());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Alerta!", ex.Message, "Cerrar");
+        }
     }
 }
--- Views/vAgregar.xaml.cs
+++ Views/vAgregar.xaml.cs
@@ -18,7 +18,8 @@ public partial class vAgregar : ContentPage
 			parametros.Add("nombre", txtNombre.Text);
 			parametros.Add("apellido", txtApellido.Text);
 			parametros.Add("edad", txtEdad.Text);
-			cliente.UploadValues("http://192.168.3.11/moviles/wsestudiantes.php", "POST", parametros);
+			//cliente.UploadValues("http://192.168.3.11/moviles/wsestudiantes.php", "POST", parametros);
+			cliente.UploadValues("http://192.168.100.109/moviles/wsestudiantes.php", "POST", parametros);
 			Navigation.PushAsync(new vEstudiante());
 		}
 		catch (Exception ex)
